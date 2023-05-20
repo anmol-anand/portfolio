448530bc3fc9cbb36d02d2e7008c14de00676704
Anmol Anand <aanand@tamu.edu>
2023-05-20 15:27:54 -0400

    initialized footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './Footer';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the content of your home page.</p>
+
+
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
