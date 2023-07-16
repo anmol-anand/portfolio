afb6329f111171f264480dc54847d9650d1a9111
Anmol Anand <aanand@tamu.edu>
2023-07-16 15:28:21 -0400

    update

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-// SOCIAL ICON
+// SOCIAL ICONS
 import SOCIAL_CODEFORCES from './assets/social-icons/codeforces.svg';
 
 function Footer({is_small_screen}) {
